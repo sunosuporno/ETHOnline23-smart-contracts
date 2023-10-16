e60a9e4d358b3f2a1c68c69ce67e7ffe79348338
sunosuporno <sarkarsuporno36@gmail.com>
2023-10-16 22:49:11 +0530

    added way to distinguish supply and other func

--- src/GoerliConnector.sol
+++ src/GoerliConnector.sol
@@ -5,8 +5,10 @@ import {IPool} from "./Ipool.sol";
 contract GoerliConnector {
 
     IPool public pool;
-    constructor(address _pool) {
+    address public daiAddress;
+    constructor(address _pool, address _daiAddress) {
         pool = IPool(_pool);
+        daiAddress = _daiAddress;
     }
 
     function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) public {
@@ -21,7 +23,17 @@ contract GoerliConnector {
         pool.borrow(asset, amount, interestRateMode, referralCode, onBehalfOf);
     }
 
+    function _executeApproval(bytes memory data) internal {
+        (bool success, bytes memory returnData) = address(daiAddress).call(data);
+        require(success, "GoerliConnector: approval failed");
+    }
+
     function _executeCall(bytes calldata data) internal{
+        (bytes memory functionData, bool isSupply) = abi.decode(data, (bytes, bool));
+        if(isSupply){
+            _executeApproval(functionData);
+            return;
+        }
         (bool success, bytes memory returnData) = address(pool).call(data);
         require(success, "GoerliConnector: call failed");
     }
